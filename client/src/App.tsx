a03eb50c7a5e8d6ae73108a1b7fcb3036913fcd1
gaurav tadi <gauravtadi11@gmail.com>
2026-09-08 17:13:57 +0530

    inital commit: project setup and basic code structure

---
+++ client/src/App.tsx
@@ -0,0 +1,10 @@
+import './App.css'
+import Header from './components/common/Header'
+
+function App() {
+ return(
+    <Header />
+ )
+}
+
+export default App
---
+++ client/src/components/common/Header.tsx
@@ -0,0 +1,11 @@
+export default function Header(){
+    return(
+        <div className=" w-full h-12 px-4 py-2 bg-[#161b22]">
+            <a href="/"><img src="/code_quest.svg" alt="code quest logo" className="h-8 w-auto"/></a>
+
+            <div className="categories">
+                
+            </div>
+        </div>
+    )
+}
